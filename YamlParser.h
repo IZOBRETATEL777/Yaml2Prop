f0942d1c239b49cd406e1ba43bd5c5a374ba1fec
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-01-01 17:47:01 +0400

    Add preliminary structure of YAML parser

---
+++ YamlParser.h
@@ -0,0 +1,16 @@
+//
+// Created by izobretatel777 on 01/01/2022.
+//
+#include <stdio.h>
+#ifndef YAML2PROP_YAMLPARSER_H
+#define YAML2PROP_YAMLPARSER_H
+
+struct YamlParser {
+    FILE* file;
+    void (*parse) (struct YamlParser*);
+};
+typedef struct YamlParser YamlParser;
+YamlParser* yamlParserConstructor(FILE* file);
+void yamlParserDestructor(YamlParser* yamlParser);
+
+#endif //YAML2PROP_YAMLPARSER_H
